489049f9bbabc10e75e22530cc41515dccecb31d
肖文龙 <joel.xiao@dclingcloud.com>
2024-02-06 21:03:53 +0800

    feat: splashscreen 开发

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,7 +5,7 @@ mod menu;
 
 fn main() {
   tauri::Builder::default()
-    .setup(|app| Ok(splashscreen::show_splashscreen(app)))
+    // .setup(|app| Ok(splashscreen::show_splashscreen(app)))
     .menu(menu::new())
     .on_menu_event(|event| menu::on_event(event))
     .invoke_handler(tauri::generate_handler![splashscreen::close_splashscreen])
--- src-tauri/src/splashscreen/mod.rs
+++ src-tauri/src/splashscreen/mod.rs
@@ -1,20 +1,20 @@
 use tauri::Manager;
 
-pub fn show_splashscreen(app: &mut tauri::App) {
-  let splashscreen_window = app.get_window("splashscreen").unwrap();
-  let main_window = app.get_window("main").unwrap();
-  // we perform the initialization code on a new task so the app doesn't freeze
-  tauri::async_runtime::spawn(async move {
-    // initialize your app here instead of sleeping :)
-    println!("Initializing...");
-    std::thread::sleep(std::time::Duration::from_secs(2));
-    println!("Done initializing.");
+// pub fn show_splashscreen(app: &mut tauri::App) {
+//   let splashscreen_window = app.get_window("splashscreen").unwrap();
+//   let main_window = app.get_window("main").unwrap();
+//   // we perform the initialization code on a new task so the app doesn't freeze
+//   tauri::async_runtime::spawn(async move {
+//     // initialize your app here instead of sleeping :)
+//     println!("Initializing...");
+//     std::thread::sleep(std::time::Duration::from_secs(2));
+//     println!("Done initializing.");
 
-    // After it's done, close the splashscreen and display the main window
-    splashscreen_window.close().unwrap();
-    main_window.show().unwrap();
-  });
-}
+//     // After it's done, close the splashscreen and display the main window
+//     splashscreen_window.close().unwrap();
+//     main_window.show().unwrap();
+//   });
+// }
 
 #[tauri::command]
 pub fn close_splashscreen(window: tauri::Window) {
