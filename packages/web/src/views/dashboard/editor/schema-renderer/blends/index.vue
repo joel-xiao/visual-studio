d4c9e6f78e4d3a10e08d57df937ee70f3207250d
肖文龙 <joel.xiao@dclingcloud.com>
2025-12-06 17:11:20 +0800

    fix(editor): fix blends component layout and remove logic

---
+++ packages/web/src/views/dashboard/editor/schema-renderer/blends/index.vue
@@ -0,0 +1,75 @@
+<template lang="pug">
+div(class="schema-blends")
+  div(class="schema-blends-item" v-for="(item, index) in list" :key="index")
+    CBlend(
+      v-model="list[index]"
+      v-bind="$attrs"
+      @update="onUpdate"
+    )
+    Button(icon="icon-remove" type="hover" @click="onRemove(index)")
+</template>
+
+<script lang="ts">
+export default {
+  name: 'BLENDS',
+  inheritAttrs: false
+};
+</script>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import CBlend from '../../../ui/controls/c-blend/index.vue';
+import Button from '../../../ui/controls/c-button/index.vue';
+
+const props = defineProps<{
+  modelValue: {
+    mix: string;
+    opacity: number;
+    visible: boolean;
+  }[];
+}>();
+
+const emit = defineEmits(['update:modelValue', 'update']);
+
+const list = ref(props.modelValue || []);
+
+watch(
+  () => props.modelValue,
+  (val) => {
+    list.value = val || [];
+  },
+  { deep: true }
+);
+
+function onUpdate() {
+  emit('update:modelValue', list.value);
+  emit('update', list.value);
+}
+
+function onRemove(index: number) {
+  list.value.splice(index, 1);
+  onUpdate();
+}
+</script>
+
+<style lang="scss">
+#dashboard .schema-blends {
+  width: 100%;
+
+  .schema-blends-item {
+    display: flex;
+    align-items: center;
+    margin-bottom: 6px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+
+    .c-blend {
+      flex: 1;
+      width: 0;
+      margin-right: 6px;
+    }
+  }
+}
+</style>
