0870f64d880a1a519b905c10b30b855752d0d43c
肖文龙 <joel.xiao@dclingcloud.com>
2025-12-15 22:28:53 +0800

    feat(ui): 添加新的 UI 基础组件
    - 添加 basic-textarea 组件
    - 添加 c-icon 组件
    - 添加 c-textarea 组件

---
+++ packages/web/src/views/dashboard/ui/controls/c-icon/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <BasicBox class="c-icon" v-bind="$attrs" :type="type">
+    <BasicIcon :icon="icon" :lock="lock" :hover="hover" :size="size" />
+  </BasicBox>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'C_ICON',
+  inheritAttrs: false
+};
+</script>
+
+<script setup lang="ts">
+import BasicBox from '../../base/basic-box.vue';
+import BasicIcon from '../../base/basic-icon.vue';
+
+interface Props {
+  icon?: string;
+  type?: string;
+  lock?: boolean;
+  hover?: boolean;
+  size?: 'small' | 'medium' | 'large';
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  icon: '',
+  type: '', // e.g. 'hover', 'button' for box styles
+  lock: false,
+  hover: false,
+  size: 'medium'
+});
+</script>
+
+<style lang="scss">
+#dashboard .c-icon {
+  justify-content: center;
+  
+  // Inherits box interactions from BasicBox based on type
+}
+</style>
