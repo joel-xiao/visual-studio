41c6d8cce22102b6872ceb1744aa11d41faa0b46
肖文龙 <joel.xiao@dclingcloud.com>
2026-02-11 15:21:32 +0800

    feat: update scatter chart schema and default configuration

--- packages/visual-craft-core/src/views/editor/materials/apache-e-charts/scatter/schema/default.ts
+++ packages/visual-craft-core/src/views/editor/materials/apache-e-charts/scatter/schema/default.ts
@@ -18,6 +18,209 @@ const schema: IComponentSchemaExport = {
   ],
   categorySchemas: [
     {
+      name: '图表',
+      icon: '',
+      category: 'CHART',
+      schemas: [
+        {
+          type: 'schema',
+          schema: 'ECHARTS_SCATTER',
+          label: '',
+          key: 'series.0',
+          default: {
+            symbolSize: [10, 10],
+            itemStyle: {
+              color: '#3662EC',
+              borderColor: '#3662EC',
+              borderWidth: 0,
+              borderType: 'solid',
+              shadowColor: '#00000000',
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              shadowBlur: 0
+            },
+            label: {
+              show: false,
+              fontFamily: 'SimSun',
+              fontSize: 12,
+              color: '#fff',
+              styles: [],
+              position: 'top'
+            }
+          }
+        }
+      ]
+    },
+    {
+      name: '标题',
+      icon: '',
+      category: 'TITLE',
+      schemas: [
+        {
+          show_switch: true,
+          type: 'schema',
+          schema: 'COMMON_TEXT',
+          label: '标题',
+          key: 'title',
+          default: {
+            value: '这是标题'
+          }
+        },
+        {
+          show_switch: true,
+          type: 'schema',
+          schema: 'COMMON_TEXT',
+          label: '副标题',
+          key: 'subtitle',
+          default: {
+            value: '这是副标题'
+          }
+        }
+      ]
+    },
+    {
+      name: '图例',
+      icon: '',
+      category: 'LEGEND',
+      schemas: [
+        {
+          show_switch: true,
+          type: 'schema',
+          schema: 'ECHARTS_LEGEND',
+          label: '图例设置',
+          key: 'legend',
+          default: {
+            textStyle: {
+              fontFamily: 'SimSun',
+              fontSize: 12,
+              color: '#fff',
+              styles: []
+            },
+            position: 'top-center',
+            icon: 'circle',
+            itemWidth: 14,
+            itemGap: 10
+          }
+        }
+      ]
+    },
+    {
+      name: '坐标轴',
+      icon: '',
+      category: 'AXIS',
+      schemas: [],
+      schemasTabs: [
+        {
+          name: 'X轴',
+          tab: 'xAxis',
+          schemas: [
+            {
+              type: 'schema',
+              schema: 'ECHARTS_AXIS',
+              label: '',
+              key: 'xAxis',
+              default: {
+                axisLabel: {
+                  fontFamily: 'Microsoft YaHei',
+                  fontSize: 12,
+                  color: '#fff',
+                  styles: [],
+                  backgroundColor: '#fff',
+                  padding: { h: 0, v: 0 }
+                },
+                splitNumber: 5,
+                axisLine: {
+                  show: true,
+                  color: '#fff',
+                  width: 1,
+                  type: 'solid'
+                },
+                axisTick: {
+                  show: true,
+                  color: '#fff',
+                  width: 1,
+                  type: 'solid',
+                  length: 5
+                },
+                splitLine: {
+                  show: false,
+                  color: '#fff',
+                  width: 1,
+                  type: 'solid'
+                }
+              }
+            }
+          ]
+        },
+        {
+          name: 'Y轴',
+          tab: 'yAxis',
+          schemas: [
+            {
+              type: 'schema',
+              schema: 'ECHARTS_AXIS',
+              label: '',
+              key: 'yAxis',
+              default: {
+                axisLabel: {
+                  fontFamily: 'Microsoft YaHei',
+                  fontSize: 12,
+                  color: '#fff',
+                  styles: [],
+                  backgroundColor: '#fff',
+                  padding: { h: 0, v: 0 }
+                },
+                splitNumber: 5,
+                axisLine: {
+                  show: true,
+                  color: '#fff',
+                  width: 1,
+                  type: 'solid'
+                },
+                axisTick: {
+                  show: true,
+                  color: '#fff',
+                  width: 1,
+                  type: 'solid',
+                  length: 5
+                },
+                splitLine: {
+                  show: true,
+                  color: '#fff',
+                  width: 1,
+                  type: 'dashed'
+                }
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: '提示框',
+      icon: '',
+      category: 'TOOLTIP',
+      schemas: [
+        {
+          show_switch: true,
+          type: 'schema',
+          schema: 'ECHARTS_TOOLTIP',
+          key: 'tooltip',
+          default: {
+            textStyle: {
+              fontFamily: 'SimSun',
+              fontSize: 12,
+              color: '#fff',
+              styles: []
+            },
+            padding: [10, 10],
+            borderColor: '#000000A6',
+            backgroundColor: '#000000A6'
+          }
+        }
+      ]
+    },
+    {
       name: '条件',
       icon: '',
       category: 'CONDITIONS',
@@ -32,6 +235,20 @@ const schema: IComponentSchemaExport = {
               schema: 'ECHARTS_SCATTER'
             }
           ]
+        }
+      ]
+    },
+    {
+      name: '自定义',
+      icon: '',
+      category: 'CODE',
+      schemas: [
+        {
+          type: 'schema',
+          schema: 'COMMON_CODE_EDITOR',
+          default: {
+            options: ''
+          }
         }
       ]
     }
--- packages/visual-craft-core/src/views/editor/schema/apache-e-charts/scatter.ts
+++ packages/visual-craft-core/src/views/editor/schema/apache-e-charts/scatter.ts
@@ -1,29 +1,143 @@
+import options from '../options';
+
 const SCHEMA_NAME = 'ECHARTS_SCATTER';
 const SCHEMA_KEY = 'scatter';
 
 const schema: ISchemaExport = {
     name: SCHEMA_NAME,
-    label: '散点图效果',
+    label: '',
     key: SCHEMA_KEY,
+    schema: [
+        [
+            {
+                label: '半径范围',
+                key: 'symbolSize',
+                type: Array,
+                default: [10, 10],
+                ctrl: 'C_COMPOUND_INPUT',
+                icon: ['mdi:arrow-expand-horizontal', 'mdi:arrow-expand-vertical'],
+                keys: ['0', '1'],
+                hint: ['宽', '高']
+            }
+        ],
+        [
+            {
+                label: '描边',
+                key: 'itemStyle',
+                type: Object,
+                default: {
+                    color: '#3662EC',
+                    borderColor: '#3662EC',
+                    borderWidth: 0,
+                    borderType: 'solid'
+                },
+                ctrl: 'C_STROKE',
+                keys: ['borderColor', 'borderWidth', 'borderType'],
+                hint: ['颜色', '粗细', '类型'],
+                options: options.LineStyleOptions,
+                suffix: 'px'
+            }
+        ],
+        [
+            {
+                label: '阴影',
+                key: 'itemStyle',
+                type: Object,
+                default: {
+                    shadowColor: '#00000000',
+                    shadowOffsetX: 0,
+                    shadowOffsetY: 0,
+                    shadowBlur: 0
+                },
+                ctrl: 'C_MIX_GROUP',
+                keys: ['shadowColor', 'shadowOffsetX', 'shadowOffsetY', 'shadowBlur'],
+                ctrls: ['C_COLOR_PICKER', 'C_INPUT', 'C_INPUT', 'C_INPUT'],
+                hints: ['颜色', 'X', 'Y', '模糊']
+            }
+        ],
+        [
+            {
+                content: '显示数据',
+                key: 'label.show',
+                type: Boolean,
+                default: false,
+                ctrl: 'C_LITE_SWITCH'
+            }
+        ],
+        [
+            {
+                hint: '字体',
+                key: 'label.fontFamily',
+                v_if: 'label.show',
+                type: String,
+                default: 'SimSun',
+                icon: 'mdi:format-font',
+                ctrl: 'C_SELECT',
+                options: options.FontFamilyOptions
+            }
+        ],
+        [
+            {
+                key: 'label.fontSize',
+                v_if: 'label.show',
+                type: Number,
+                default: 12,
+                hint: '字号',
+                icon: 'mdi:format-size',
+                ctrl: 'C_INPUT',
+                size: 'large'
+            },
+            {
+                key: 'label.color',
+                v_if: 'label.show',
+                type: String,
+                default: '#fff',
+                hint: '颜色',
+                ctrl: 'C_COLOR_PICKER',
+                size: 'mini'
+            }
+        ],
+        [
+            {
+                key: 'label.styles',
+                v_if: 'label.show',
+                type: Array,
+                default: [],
+                ctrl: 'C_GROUP_CHECK',
+                options: options.TextStyleOptions
+            }
+        ],
+        [
+            {
+                key: 'label.position',
+                v_if: 'label.show',
+                type: String,
+                default: 'top',
+                hint: '对齐',
+                icon: 'mdi:format-align-center',
+                ctrl: 'C_SELECT',
+                size: 'wide',
+                options: options.BarLabelPositionOptions
+            }
+        ]
+    ],
     conditionsSchema: [
         [
             {
                 label: '散点填充颜色',
-                key: 'color',
+                key: 'itemStyle.color',
                 ctrl: 'C_COLOR_PICKER',
                 type: String,
                 default: '#3662EC',
                 ctrl_type: 'color-input',
-                size: 'small'
             },
             {
                 label: '描边/边框颜色',
-                key: 'borderColor',
+                key: 'itemStyle.borderColor',
                 ctrl: 'C_COLOR_PICKER',
                 type: String,
                 default: '#3662EC',
                 ctrl_type: 'color-input',
-                size: 'small'
             }
         ]
     ]
